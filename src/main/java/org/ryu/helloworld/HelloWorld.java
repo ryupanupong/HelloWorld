b4ac4a570a9832123a78d8a80a513b293ef33e18
ryupanupong <ryupc5940@hotmail.com>
2020-02-29 00:40:04 +0700

    init 2

--- src/main/java/org/ryu/helloworld/HelloWorld.java
+++ src/main/java/org/ryu/helloworld/HelloWorld.java
@@ -10,5 +10,7 @@ package org.ryu.helloworld;
  * @author USER
  */
 public class HelloWorld {
-    
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+    }
 }
